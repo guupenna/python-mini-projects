c46400c60954649c571e1d100f8aa1e8501f26bd
Gustavo <gupenna1@gmail.com>
2026-05-20 00:44:59 -0300

    selection by tournament method done

--- ga/main.py
+++ ga/main.py
@@ -1,15 +1,28 @@
 import numpy as np
-import random
 
 def fitness(population):
     score = np.sum(population, axis=1)
     return score
 
-def selection(population):
-    return random.choices(population, k=2)
+def selection(population, scores):
+    tournament = np.random.choice(len(population), size=(len(population), 2))
+
+    winner_tournament = np.argmax(scores[tournament], axis=1)
+    
+    selected = tournament[np.arange(len(population)), winner_tournament]
+
+#    selected = []
+#    for i in range(0, len(population)):
+#        tournament = np.random.choice(len(population), size=2, replace=False)
+#        if (scores[tournament[0]] > scores[tournament[1]]):
+#            selected.append(tournament[0])
+#        else:
+#            selected.append(tournament[1])
+
+    return population[selected]
 
 population = np.random.randint(low=0, high=2, size=(10, 5))
 
-score = fitness(population)
+scores = fitness(population)
 
-# print(population)
+print(selection(population, scores))
